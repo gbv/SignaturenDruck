f782cad23b5565c5d4bc3edb37cb2b9fa7e25cf1
Elias <elias.dehler@thulb.uni-jena.de>
2018-05-09 12:48:12 +0200

    working printing with count

--- signaturenDruck/printRenderer.js
+++ signaturenDruck/printRenderer.js
@@ -23,7 +23,8 @@ const ipc = require("electron").ipcRenderer;
 window.onload = function () {
     ipc.on("toPrint", function(event, data){
         console.log("on printRenderer", event, data);
-        preview(data);
+        createPage(data);
+        createPDF();
     });
 };
 
@@ -43,69 +44,7 @@ function loadConfig() {
     return config.store;
 }
 
-function preview(ids) {
-    // fs.readFile("signaturen.json", "utf8", function readFileCallback(err, data){
-    //     if (err){
-    //         console.log(err);
-    //     } else {
-    //         console.log("ids: ", ids);
-    //         addUsername();
-    //         addDate();
-    //         // needs to sum up all the counts per id
-    //         let countAll = 0;
-    //         let idNr = 1;
-    //         _.forEach((ids), function(value){
-    //             if (value.count >= 1 && value.count <= 99) {
-    //                 countAll += Number(value.count);
-    //             } else {
-    //                 countAll += 1;
-    //             }
-                
-    //         });
-    //         console.log(countAll);
-    //         _.forEach(ids, function(value){
-    //             console.log("value: ", value);
-    //             let objct = value;
-    //             _.forEach(JSON.parse(data), function(key, value){
-    //                 let sig = "";
-    //                 let found = _.find(key, {"id": Number(objct.id)});
-    //                 console.log(objct.id);
-    //                 if (found !== undefined) {
-    //                     sig = found;
-    //                 }
-    //                 if (sig != "") {
-    //                     let length = sig.txtLength;
-    //                     let id = sig.id;
-    //                     let i = 1;
-    //                     let div = document.createElement("div");
-    //                     let line = document.createElement("p");
-    //                     div.className = "shelfmark";
-    //                     div.id = id;
-    //                     sig.txt.forEach(element => {
-    //                         line.className = "shelfmarkLine_" + i;
-    //                         if (element == "") {
-    //                             let emptyLine = document.createElement("br");
-    //                             line.appendChild(emptyLine);
-    //                         } else {
-    //                             line.innerHTML = element;
-    //                         }
-    //                         div.appendChild(line);
-    //                         line = document.createElement("p");
-    //                         i++;
-    //                     });
-    //                     document.getElementById("toPrint").appendChild(div);
-    //                 }
-    //                 if (idNr < countAll) {
-    //                     let pdfPageBreak = document.createElement("div");
-    //                     pdfPageBreak.className = "html2pdf__page-break";
-    //                     document.getElementById("toPrint").appendChild(pdfPageBreak);
-    //                 }
-    //                 idNr++;
-    //             });
-    //         });
-    //     }
-    //     createPDF();
-    // });
+function createPage(ids) {
     let file = fs.readFileSync("signaturen.json", "utf8");
     console.log("ids: ", ids);
     addUsername();
@@ -132,39 +71,38 @@ function preview(ids) {
                 sig = found;
             }
             if (sig != "") {
-                let length = sig.txtLength;
-                let id = sig.id;
-                let i = 1;
-                let div = document.createElement("div");
-                let line = document.createElement("p");
-                div.className = "shelfmark";
-                div.id = id;
-                sig.txt.forEach(element => {
-                    line.className = "shelfmarkLine_" + i;
-                    if (element == "") {
-                        let emptyLine = document.createElement("br");
-                        line.appendChild(emptyLine);
-                    } else {
-                        line.innerHTML = element;
+                for (let count = 0; count < objct.count; count++) {
+                    let length = sig.txtLength;
+                    let id = sig.id;
+                    let i = 1;
+                    let div = document.createElement("div");
+                    let line = document.createElement("p");
+                    div.className = "shelfmark";
+                    div.id = id;
+                    sig.txt.forEach(element => {
+                        line.className = "shelfmarkLine_" + i;
+                        if (element == "") {
+                            let emptyLine = document.createElement("br");
+                            line.appendChild(emptyLine);
+                        } else {
+                            line.innerHTML = element;
+                        }
+                        div.appendChild(line);
+                        line = document.createElement("p");
+                        i++;
+                    });
+                    document.getElementById("toPrint").appendChild(div);
+                    console.log("idNr: ", idNr, "countAll: ", countAll);
+                    if (idNr < countAll) {
+                        let pdfPageBreak = document.createElement("div");
+                        pdfPageBreak.className = "html2pdf__page-break";
+                        document.getElementById("toPrint").appendChild(pdfPageBreak);
+                        idNr++;
                     }
-                    div.appendChild(line);
-                    line = document.createElement("p");
-                    i++;
-                });
-                document.getElementById("toPrint").appendChild(div);
-                console.log("idNr: ", idNr, "countAll: ", countAll);
-                if (idNr < countAll) {
-                    let pdfPageBreak = document.createElement("div");
-                    pdfPageBreak.className = "html2pdf__page-break";
-                    document.getElementById("toPrint").appendChild(pdfPageBreak);
-                    idNr++;
                 }
             }
         });
     });
-
-    createPDF();
-
 }
 
 function addUsername() {
@@ -196,7 +134,7 @@ function createPDF() {
         margin:       1,
         filename:     "C://Export/myfile.pdf",
         image:        { type: "png", quality: 1 },
-        html2canvas:  { dpi: 600, letterRendering: true },
+        html2canvas:  { dpi: 300, letterRendering: true },
         jsPDF:        { unit: newConfig.einheit, format: [newConfig.gross.label.width + 0.5, newConfig.gross.label.height + 0.5], orientation: "landscape" }
     });
 }
