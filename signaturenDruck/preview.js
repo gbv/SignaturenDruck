2bebe350e96567b6c25ffa7afae9a1dd7eddda3f
EliDeh <elias.dehler@thulb.uni-jena.de>
2018-08-24 14:33:54 +0200

    fixed a bug related to the new sortByPPN config switch

--- signaturenDruck/preview.js
+++ signaturenDruck/preview.js
@@ -3,9 +3,16 @@ const fs = require('fs')
 // requires lodash
 const _ = require('lodash')
 
+// requires the electron-store module and initializes it
+const Store = require('electron-store')
+const config = new Store({cwd: 'C:\\Export\\SignaturenDruck'})
+
+let isSmall = false
+let isShort = false
+
 module.exports = function (id) {
-  let isSmall = false
-  let isShort = false
+  isSmall = false
+  isShort = false
   if (document.getElementById('short_' + id)) {
     isSmall = true
     if (document.getElementById('short_' + id).checked) {
@@ -34,109 +41,121 @@ module.exports = function (id) {
     myNode.removeChild(myNode.firstChild)
   }
   let file = fs.readFileSync('signaturen.json', 'utf8')
-  _.forEach(JSON.parse(file), function (key, value) {
-    let sig = ''
-    let found = _.find(key, { 'id': Number(id) })
+  if (config.store.sortByPPN) {
+    _.forEach(JSON.parse(file), function (key, value) {
+      let sig = ''
+      let found = ''
+      found = _.find(key, { 'id': Number(id) })
+      if (found !== undefined) {
+        sig = found
+        showData(sig)
+      }
+    })
+  } else {
+    let found = _.find(JSON.parse(file), { 'id': Number(id) })
     if (found !== undefined) {
-      sig = found
+      showData(found)
     }
-    if (sig != '') {
-      let length = sig.txtLength
-      let id = sig.id
-      let i = 1
-      let div = document.createElement('div')
-      let line = document.createElement('p')
-      div.className = 'shelfmark center'
-      if (isSmall) {
-        if (isShort || length != 1) {
-          div.className = 'shelfmark indent'
-        }
+  }
+}
+
+function showData (sig) {
+  if (sig != '') {
+    let length = sig.txtLength
+    let id = sig.id
+    let i = 1
+    let div = document.createElement('div')
+    let line = document.createElement('p')
+    div.className = 'shelfmark center'
+    if (isSmall) {
+      if (isShort || length != 1) {
+        div.className = 'shelfmark indent'
       }
-      div.id = id
-      if (document.getElementById('short_' + id)) {
-        if (document.getElementById('short_' + id).checked && length == 1) {
-          let text = sig.txt[0]
-          let indxSlash = text.indexOf('/')
-          let indxColon = text.indexOf(':')
-          sig.txt = []
-          sig.txt[0] = text
-          let i = 0
-          if (indxSlash !== -1) {
-            sig.txt[0] = text.substring(0, indxSlash + 1)
-            sig.txt[1] = text.substring(indxSlash + 1)
+    }
+    div.id = id
+    if (document.getElementById('short_' + id)) {
+      if (document.getElementById('short_' + id).checked && length == 1) {
+        let text = sig.txt[0]
+        let indxSlash = text.indexOf('/')
+        let indxColon = text.indexOf(':')
+        sig.txt = []
+        sig.txt[0] = text
+        let i = 0
+        if (indxSlash !== -1) {
+          sig.txt[0] = text.substring(0, indxSlash + 1)
+          sig.txt[1] = text.substring(indxSlash + 1)
+          i = 1
+        }
+        if (indxColon !== -1) {
+          if (i === 0) {
+            sig.txt[0] = text.substring(0, indxColon + 1)
+            sig.txt[1] = text.substring(indxColon + 1)
             i = 1
-          }
-          if (indxColon !== -1) {
-            if (i === 0) {
-              sig.txt[0] = text.substring(0, indxColon + 1)
-              sig.txt[1] = text.substring(indxColon + 1)
-              i = 1
-            } else {
-              let i = 0
-              let txt = []
-              let length = sig.txt.length
-              sig.txt.forEach(element => {
-                let indx = element.indexOf(':')
-                if (indx !== -1) {
-                  let j = 0
-                  while (j < i) {
-                    txt[j] = sig.txt[j]
-                    j++
-                  }
-                  let k = i
-                  txt[k] = element.substring(0, indx)
-                  k++
-                  txt[k] = element.substring(indx)
-                  k++
-                  while (k <= length) {
-                    txt[k] = sig.txt[k - 1]
-                    k++
-                  }
-                  sig.txt = txt
+          } else {
+            let i = 0
+            let txt = []
+            let length = sig.txt.length
+            sig.txt.forEach(element => {
+              let indx = element.indexOf(':')
+              if (indx !== -1) {
+                let j = 0
+                while (j < i) {
+                  txt[j] = sig.txt[j]
+                  j++
                 }
-                i++
-              })
-            }
-          }
-          i = 0
-          let txt = []
-          let length = sig.txt.length
-          sig.txt.forEach(element => {
-            let elementParts = element.split(' ')
-            if (elementParts.length >= 3) {
-              let j = 0
-              while (j < i) {
-                txt[j] = sig.txt[j]
-                j++
-              }
-              let k = i
-              txt[k] = elementParts[0] + ' ' + elementParts[1]
-              k++
-              txt[k] = element.substring(txt[k - 1].length)
-              k++
-              while (k <= length) {
-                txt[k] = sig.txt[k - 1]
+                let k = i
+                txt[k] = element.substring(0, indx)
                 k++
+                txt[k] = element.substring(indx)
+                k++
+                while (k <= length) {
+                  txt[k] = sig.txt[k - 1]
+                  k++
+                }
+                sig.txt = txt
               }
-              sig.txt = txt
+              i++
+            })
+          }
+        }
+        i = 0
+        let txt = []
+        let length = sig.txt.length
+        sig.txt.forEach(element => {
+          let elementParts = element.split(' ')
+          if (elementParts.length >= 3) {
+            let j = 0
+            while (j < i) {
+              txt[j] = sig.txt[j]
+              j++
             }
-            i++
-          })
-        }
+            let k = i
+            txt[k] = elementParts[0] + ' ' + elementParts[1]
+            k++
+            txt[k] = element.substring(txt[k - 1].length)
+            k++
+            while (k <= length) {
+              txt[k] = sig.txt[k - 1]
+              k++
+            }
+            sig.txt = txt
+          }
+          i++
+        })
       }
-      sig.txt.forEach(element => {
-        line.className = 'previewLine'
-        line.id = 'line' + i
-        if (element == '') {
-          let emptyLine = document.createElement('br')
-          line.appendChild(emptyLine)
-        } else {
-          line.innerHTML = element
-        }
-        document.getElementById('previewBox').appendChild(line)
-        line = document.createElement('p')
-        i++
-      })
     }
-  })
+    sig.txt.forEach(element => {
+      line.className = 'previewLine'
+      line.id = 'line' + i
+      if (element == '') {
+        let emptyLine = document.createElement('br')
+        line.appendChild(emptyLine)
+      } else {
+        line.innerHTML = element
+      }
+      document.getElementById('previewBox').appendChild(line)
+      line = document.createElement('p')
+      i++
+    })
+  }
 }
