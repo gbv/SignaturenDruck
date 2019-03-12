4dedc7cb9c3fe01db3b10c6d8037dde39862a062
EliDeh <elias.dehler@thulb.uni-jena.de>
2019-03-12 14:49:40 +0100

    added warning if printer name contains '-'

--- signaturenDruck/js/classes/Printers.js
+++ signaturenDruck/js/classes/Printers.js
@@ -37,9 +37,13 @@ class Printers {
 
   checkPrinters () {
     let printerList = this.getPrinterNameList()
+    let printerNameError = []
     for (let format in this.formats) {
       if (this.formats.hasOwnProperty(format)) {
         this._printers[format] = Printers.isIncluded(this.formats[format].printer, printerList)
+        if (/(\s-\s?)/.test(this.formats[format].printer) || /(\s?-\s)/.test(this.formats[format].printer)) {
+          printerNameError.push(format)
+        }
       }
     }
     let printerNotFound = []
@@ -51,16 +55,30 @@ class Printers {
       }
     }
     let str = ''
-    if (printerNotFound.length > 0) {
+    if (printerNotFound.length > 0 || printerNameError.length > 0) {
       if (printerNotFound.length === 1) {
         str = 'Der Drucker des Formats: "' + printerNotFound[0] + '" wurde nicht gefunden'
-      } else {
+      } else if (printerNotFound > 1) {
         str = 'Die Drucker der folgenden Formate wurden nicht gefunden: "'
         printerNotFound.forEach(element => {
           str += element + ', '
         })
         str = str.substr(0, str.length - 2)
         str += '"'
+      }
+      if (str !== '') {
+        str += '<br/>'
+      }
+      if (printerNameError.length === 1) {
+        str += 'Der Druckername des Formats: "' + printerNameError[0] + '" enthält ein "-", dies kann zu einem Problem beim drucken führen.'
+      } else {
+        str += 'Die Druckernamen der folgenden Formate enthalten ein "-": <br/>"'
+        printerNameError.forEach(element => {
+          str += element + ', '
+        })
+        str = str.substr(0, str.length - 2)
+        str += '"<br/>'
+        str += 'Dies kann zu einem Problem beim drucken führen.'
       }
       document.getElementById('infoBox').insertAdjacentHTML('afterbegin', '<div class="notification is-warning">' + str + '</div>')
     }
